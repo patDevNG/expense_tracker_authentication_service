1f8760e40f2208cc244a54e7ff0a0d188ec59e6a
Okuns Patrick <patrick.okuns@skuad.io>
2021-06-27 22:18:23 +0100

    refactor: added the controller interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,9 @@
 import { HttpRequest, HttpResponse } from '../protocol/http'
 import { MissingParamsError } from '../error/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-export class SignUpController {
+import { Controller } from '../protocol/controller'
+
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const compulsoryField = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of compulsoryField) {
