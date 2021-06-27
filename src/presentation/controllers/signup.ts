bb95a3007c2d3daef231dc32a240897cc2af1085
Okuns Patrick <patrick.okuns@skuad.io>
2021-06-27 21:46:04 +0100

    refactor: moved duplicated code to a helper function

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,13 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { HttpRequest, HttpResponse } from '../protocol/http'
 import { MissingParamsError } from '../error/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamsError('name')
-      }
+      return badRequest(new MissingParamsError('name'))
     } else if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamsError('email')
-      }
+      return badRequest(new MissingParamsError('email'))
     } else {
       return {
         statusCode: 200,
