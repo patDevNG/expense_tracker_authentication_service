414c2d07017925d687de21bc71e7e3c720fe9a87
Okuns Patrick <patrick.okuns@skuad.io>
2021-06-27 22:00:04 +0100

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,15 +4,16 @@ import { MissingParamsError } from '../error/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamsError('name'))
-    } else if (!httpRequest.body.email) {
-      return badRequest(new MissingParamsError('email'))
-    } else {
-      return {
-        statusCode: 200,
-        body: 'Ok'
+    const compulsoryField = ['name', 'email']
+    for (const field of compulsoryField) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field))
       }
+    }
+
+    return {
+      statusCode: 200,
+      body: 'Ok'
     }
   }
 }
