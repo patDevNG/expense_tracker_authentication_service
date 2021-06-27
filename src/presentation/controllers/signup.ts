22ee95c29cf189b650d0002532aa911e50fe1264
Okuns Patrick <patrick.okuns@skuad.io>
2021-06-27 21:05:43 +0100

    refactor: refactor signup controller to have a return type that matches declaration

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,17 +1,21 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-
+import { HttpRequest, HttpResponse } from '../protocol/http'
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
-    }
-    if (!httpRequest.body.email) {
+    } else if (!httpRequest.body.email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
+      }
+    } else {
+      return {
+        statusCode: 200,
+        body: 'Ok'
       }
     }
   }
