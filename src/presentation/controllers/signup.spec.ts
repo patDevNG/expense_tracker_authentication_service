b62c9af9efe7400bd6ef7f0e5d338a8f0c6f1787
Okuns Patrick <patrick.okuns@skuad.io>
2021-06-29 21:28:15 +0100

    change mocking of the email validator for signup controller

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -9,7 +9,7 @@ interface sutTypes {
   emailValidatorStub: EmailValidator
 }
 
-const makeEmailValidtor = (): EmailValidator => {
+const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid (email: string): boolean {
       return true
@@ -18,16 +18,8 @@ const makeEmailValidtor = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidtorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
 const makeSut = (): sutTypes => {
-  const emailValidatorStub = makeEmailValidtor()
+  const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
   return {
     sut,
@@ -123,8 +115,10 @@ describe('SignUp Controller', () => {
   })
 
   test('should return 500 if EmailValidator throws erro ', () => {
-    const emailValidatorStub = makeEmailValidtorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
     const httpRequest = {
       body: {
         name: 'any_name',
